9480716922e331e6c2cadb88d15a0c8f140d36ac
Mitz Bitz <mitchellvieira12@gmail.com>
2025-05-21 15:37:35 -0400

    major css changes

--- LightModeContext.jsx
+++ LightModeContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 const LightModeContext = createContext();
 
 export function LightModeProvider({ children }) {
-  const [lightMode, setLightmode] = useState(true);
+  const [lightMode, setLightmode] = useState(false);
   return (
     <LightModeContext.Provider value={{ lightMode, setLightmode }}>
       {children}
